ea3a6b9dec922db5d1e4629c7f2171555ae12671
Ryan Rasi <29680273+RyanRasi@users.noreply.github.com>
2023-09-20 19:34:06 +0100

    API to Frontend Card

--- movie-match/src/app/data.service.ts
+++ movie-match/src/app/data.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject, forkJoin, Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 
 
@@ -11,10 +11,55 @@ export class DataService {
   public recommendation$ = new BehaviorSubject<any[]>([]);
   params = '';
   // Set a loading flag to indicate that a request is in progress
-isLoading = true;
+  private isLoading = false;
+  public isLoading$ = new BehaviorSubject<boolean>(false);
+  movie_metadata_path = 'assets/movies_metadata.csv';
+  movieIds_path = 'assets/movie.csv';
+  movies: any[] = [];
 
 constructor(private http: HttpClient) { }
 
+  readCSV(): void {
+    const movieMetadata$ = this.http.get(this.movie_metadata_path, { responseType: 'text' });
+    const movieIds$ = this.http.get(this.movieIds_path, { responseType: 'text' });
+
+    forkJoin([movieMetadata$, movieIds$]).subscribe(([movieMetadata, movieIds]) => {
+      this.extractColumnValues(movieMetadata, movieIds);
+    });
+  }
+
+  extractColumnValues(csvData1: string, csvData2: string): void {
+    const rows1 = csvData1.split('\n');
+    const rows2 = csvData2.split('\n');
+  
+    const movieMap = new Map();
+
+    // Parse the second CSV file to create a mapping of movie titles to movie IDs
+    for (let i = 1; i < rows2.length; i++) {
+      const columns = rows2[i].split(',');
+      if (columns.length > 1) {
+        const movieId = columns[0].trim().replace(/"/g, '');;
+        const movieTitle = columns[1].trim().replace(/"/g, '');;
+        movieMap.set(movieTitle, movieId);
+      }
+    }
+
+    // Parse the first CSV file and match movie titles to movie IDs
+    for (let i = 1; i < rows1.length; i++) {
+      const columns = rows1[i].split(',');
+      if (columns.length > 1) {
+        const movieTitle = columns[0].trim();
+        const moviePoster = columns[1].trim();
+        const movieReleaseDate = columns[3].trim().split("-")[0];
+  
+        // Check if the movie title exists in the mapping
+        if (movieMap.has(movieTitle + ' (' + movieReleaseDate + ')')) {
+          const movieId = movieMap.get(movieTitle + ' (' + movieReleaseDate + ')');
+          this.movies.push({ title: movieTitle, year: movieReleaseDate, poster: moviePoster, movie_id: movieId });
+        }
+      }
+    }
+  }
 
   addData(item: any): void {
     this.data.push(item);
@@ -38,6 +83,7 @@ constructor(private http: HttpClient) { }
   }
 
   getRecommendations(requestMovies: any[]) {
+    this.readCSV();
     this.params = '';
     for (let i = 0; i < requestMovies.length; i++) {
       this.params = this.params + 'movieIds=' + requestMovies[i] + '&'
@@ -46,24 +92,36 @@ constructor(private http: HttpClient) { }
     this.params = this.params.slice(0, -1);
 
     console.log('http://localhost:8080/recommendHybrid?' + this.params);
-
+    this.isLoading$.next(true);
 // Call URL
 this.http.get('http://localhost:8080/recommendHybrid?' + this.params)
   .subscribe(
     (data: any) => {
       // Handle the API response data - Read the csv, save the columns, and search for the movie ID
-      console.log(data);
+      console.log('Recommendations from API: ' + data);
+      for (let i = 0; i < data.length; i ++) {
+        
+        console.log('Checking for recommended movie in csv: ' + data[i]);
+
+        let filteredMovies = this.movies.filter(movie => movie.movie_id == data[i]);
+        if (filteredMovies.length != 0) {
+          console.log('csv match found: ' + filteredMovies);
+
+          this.recommendation.push(filteredMovies[0]);
+          this.recommendation$.next(this.recommendation);
+        } else {
+          console.log('csv contains broken match')
+        }
+      }
       // Turn off the loading indicator when the request is complete
-      this.isLoading = false;
+      this.isLoading$.next(false);
     },
     (error: any) => {
       // Handle any errors
       console.error(error);
       // Turn off the loading indicator on error as well
-      this.isLoading = false;
+      this.isLoading$.next(false);
     }
   );
-    this.recommendation.push({ title: 'Cars', year: '2006', poster: 'https://image.tmdb.org/t/p/original/u4G8EkiIBZYx0wEg2xDlXZigTOZ.jpg' });
-    this.recommendation$.next(this.recommendation);
   }
 }
